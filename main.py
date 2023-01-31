cdcd6e5aea3e35937f96d8785304df2d99742291
Nate Rush <naterush1997@gmail.com>
2023-01-31 10:34:30 -0800

    mitosheet: add mrr calculations

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import json
+import logging
 import os
 from datetime import datetime, timedelta
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Literal, Optional, Tuple
 
 import numpy as np
 import pandas as pd
 import plotly.express as px
 import requests
+import snowflake.connector
 import streamlit as st
 from dateutil import rrule
 
+logging.getLogger('snowflake.connector').setLevel(logging.WARNING)
+logging.getLogger('stripe').setLevel(logging.WARNING)
+
 
 def get_secret(key):
+    import os
+
+    import streamlit as st
+    
     if key in os.environ:
         return os.environ[key]
     else:
         return st.secrets[key]
- 
 
 def get_mixpanel_query(payload: str, headers: Dict[str, str]) -> Dict[str, Any]:
     service_account_username = get_secret('MIXPANEL_SERVICE_ACCOUNT_USERNAME')
@@ -86,67 +94,22 @@ def get_mixpanel_signup_data(use_mixpanel_cache):
 
     return df
 
-def do_brex_api_call(path, next_cursor=None) -> Tuple[List, Optional[str]]:
-    url = "https://platform.brexapis.com/v2/" + path + ('' if next_cursor is None else f'?cursor={next_cursor}')
+def get_snowflake_table_as_df(schema: str, table: str) -> pd.DataFrame:
+    con = snowflake.connector.connect(
+        user=get_secret('SNOWFLAKE_USERNAME'), 
+        password=get_secret('SNOWFLAKE_PASSWORD'), 
+        account=get_secret('SNOWFLAKE_ACCOUNT'), 
+        warehouse='COMPUTE_WH', 
+        database='DASHBOARD_DATA', 
+        schema=schema
+    )
 
-    headers = {"Authorization": f"Bearer {get_secret('BREX_API_TOKEN')}"}
-
-    response = requests.get(url, headers=headers)
-
-    data = response.json()
-    return data['items'], data['next_cursor'] if 'next_cursor' in data else None
-
-
-use_brex_transaction_cache = True
-BREX_TRANSACTION_DATA_PATH = 'brex_transaction_data.csv'
-
-def get_brex_transaction_data():
-
-    if os.path.exists(BREX_TRANSACTION_DATA_PATH) and use_brex_transaction_cache:
-        df = pd.read_csv(BREX_TRANSACTION_DATA_PATH)
-    else:
-        path = 'transactions/cash/' + get_secret('BREX_CASH_ACCOUNT_ID')
-        data, cursor = do_brex_api_call(path)
-        while cursor is not None:
-            new_data, cursor = do_brex_api_call(path, cursor)
-            data.extend(new_data)
-
-        # We go through and parse our amount and currency
-        for d in data:
-            d['currency'] = d['amount']['currency']
-            d['amount'] = float(d['amount']['amount']) / 100
-            d['month'] = pd.to_datetime(d['posted_at_date']).replace(day=1)
-
-        df = pd.DataFrame(data)    
-        df.to_csv(BREX_TRANSACTION_DATA_PATH, index=False)
-    
+    cur = con.cursor()
+    cur.execute(f'SELECT * FROM {table}')
+    df = cur.fetch_pandas_all()
+    df.columns = [col.lower() for col in df.columns]
     return df
 
-
-use_brex_account_cache = True
-BREX_ACCOUNT_DATA_PATH = 'brex_account_data.csv'
-
-def get_brex_account_data():
-    if os.path.exists(BREX_ACCOUNT_DATA_PATH) and use_brex_account_cache:
-        df = pd.read_csv(BREX_ACCOUNT_DATA_PATH)
-    else:
-        path = "accounts/cash/" + get_secret('BREX_CASH_ACCOUNT_ID') + "/statements"
-        data, cursor = do_brex_api_call(path)
-        while cursor is not None:
-            new_data, cursor = do_brex_api_call(path, cursor)
-            data.extend(new_data)
-
-        for d in data:
-            d['start_date'] = d['period']['start_date']
-            d['end_date'] = d['period']['end_date']
-            d['start_balance'] = float(d['start_balance']['amount']) / 100
-            d['end_balance'] = float(d['end_balance']['amount']) / 100
-            d['burn'] = d['start_balance'] - d['end_balance']
-
-        df = pd.DataFrame(data)
-        df.to_csv(BREX_ACCOUNT_DATA_PATH, index=False)
-
-    return df
 
 def get_runway_string(balance: float, burn: float) -> str:
     if burn < 0:
@@ -172,29 +135,131 @@ def get_is_default_alive(starting_balance: float, revenue: float, expenses: floa
 
     return False
 
+stripe_cache = 'stripe_subscriptions.csv'
+
+def get_stripe_subscriptions():
+    if os.path.exists(stripe_cache):
+        df = pd.read_csv(stripe_cache)
+        df['start_date'] = pd.to_datetime(df['start_date'])
+        df['end_date'] = pd.to_datetime(df['end_date'], errors='ignore')
+        return df
+    else:
+        import stripe
+        stripe.api_key = get_secret('STRIPE_KEY')
+
+        # We list all subscripts all time
+        subscriptions = []
+        current = stripe.Subscription.list(status='all')['data']
+        while len(current) > 0:
+            subscriptions.extend(current)
+            current = stripe.Subscription.list(status='all', limit=100, starting_after=subscriptions[-1]['id'])['data']
+
+        start_date = []
+        end_date = []
+        price = []
+        for subscription in subscriptions:
+            start_date.append(datetime.fromtimestamp(subscription['start_date']))
+            end_date.append(datetime.fromtimestamp(subscription['ended_at']) if subscription['ended_at'] is not None else None)
+            price.append(subscription['plan']['amount'] / 100)
+
+        df = pd.DataFrame({'start_date': start_date, 'end_date': end_date, 'amount': price})
+        df.to_csv(stripe_cache, index=False)
+    return df
+
+def get_stripe_subscriptions_at_time(stripe_subscriptions: pd.DataFrame, dt: datetime) -> pd.DataFrame:
+    return stripe_subscriptions[(stripe_subscriptions['start_date'] < dt) & ((stripe_subscriptions['end_date'].isna()) | (stripe_subscriptions['end_date'] >= dt))]
+
+def get_team_customers_at_time(team_customer_data: pd.DataFrame, dt: datetime) -> pd.DataFrame:
+    return team_customer_data[(team_customer_data['start_date'] < dt) & (team_customer_data['end_date'] >= dt)]
+
+def get_revenue_and_customers_dataframe(stripe_subscriptions: pd.DataFrame, team_customer_data: pd.DataFrame, mrr_or_arr: Literal['MRR', 'ARR']) -> pd.DataFrame:    
+    start_date = datetime.now() - timedelta(weeks=52)
+    end_date = datetime.now()
+
+    times = []
+    stripe_revenues = []
+    stripe_subscription_count = []
+    teams_revenue = []
+    teams_subscription_count = []
+    total_revenue = []
+
+    for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
+        
+        stripe_subs = get_stripe_subscriptions_at_time(stripe_subscriptions, dt)
+        team_subs = get_team_customers_at_time(team_customer_data, dt)
+
+        times.append(dt)
+        stripe_revenues.append(stripe_subs['amount'].sum())
+        stripe_subscription_count.append(len(stripe_subs))
+        teams_revenue.append(team_subs['monthly_amount'].sum())
+        teams_subscription_count.append(len(team_subs))
+        total_revenue.append(stripe_revenues[-1] + teams_revenue[-1])
+
+    if mrr_or_arr == 'ARR':
+        stripe_revenues = list(map(lambda x: x * 12, stripe_revenues))
+        teams_revenue = list(map(lambda x: x * 12, teams_revenue))
+        total_revenue = list(map(lambda x: x * 12, total_revenue))
+
+    return pd.DataFrame({
+        'time': times,
+        'stripe_revenue': stripe_revenues,
+        'stripe_subscription_count': stripe_subscription_count,
+        'team_revenue': teams_revenue,
+        'team_subscription_count': teams_subscription_count,
+        'total_revenue': total_revenue
+    })
+
 st.title('Mito Company Dashboard')
 
-financial_tab, mixpanel_tab = st.tabs(["Financials", "Mixpanel"])
+revenue_tab, expense_tab, mixpanel_tab = st.tabs(["Revenue", "Expenses", "Mixpanel"])
 
-with financial_tab:
-    st.header("Brex Data")
-    brex_transaction_data = get_brex_transaction_data()
-    brex_account_data = get_brex_account_data()
+with revenue_tab:
+    brex_transaction_data = get_snowflake_table_as_df('BREX', 'TRANSACTION_DATA')
+    brex_account_data = get_snowflake_table_as_df('BREX', 'ACCOUNT_DATA')
+    team_customer_data = get_snowflake_table_as_df('TEAMS', 'CUSTOMERS')
+    stripe_subscriptions = get_stripe_subscriptions()
+
+    st.header('Revenue')
+
+    mrr_or_arr: Literal['MRR', 'ARR'] = st.selectbox('MRR or ARR', ['MRR', 'ARR']) # type: ignore
+    revenue_per_month = get_revenue_and_customers_dataframe(stripe_subscriptions, team_customer_data, mrr_or_arr)
+    st.plotly_chart(
+        px.bar(
+            revenue_per_month, x='time', y=['stripe_revenue', 'team_revenue', 'total_revenue'], 
+            barmode="group",
+            title=f"Revenue ({mrr_or_arr})", labels={'value': mrr_or_arr, 'variable': ''},
+        )
+    )
 
     # We only look at recent revenue to avoid investment
-    recent_revenue = brex_transaction_data[(brex_transaction_data['amount'] >= 0) & (brex_transaction_data['initiated_at_date'] >= '2022-10-01')]
+    recent_revenue = brex_transaction_data[(brex_transaction_data['amount'] >= 0) & (brex_transaction_data['initiated_at_date'] >= pd.to_datetime('2022-10-01'))]
     recent_revenue_summed = recent_revenue.groupby('month').sum(numeric_only=True).reset_index().sort_values(by='month', ascending=False)
-    st.plotly_chart(px.bar(recent_revenue_summed, x='month', y='amount', title='Recent Revenue'))
+    st.plotly_chart(px.bar(recent_revenue_summed, x='month', y='amount', title='Actual Income (Money Entering Bank Account)'))
 
+    st.header("Revenue Breakdown")
+
+    current_team_customers = team_customer_data[team_customer_data['end_date'] >= datetime.now()]
+    current_teams_revenue = current_team_customers['monthly_amount'].sum()
+
+    st.subheader("Revenue and Subscribers Over Time")
+    revenue_per_month = get_revenue_and_customers_dataframe(stripe_subscriptions, team_customer_data, 'MRR')
+    st.dataframe(revenue_per_month)
+    
+    st.subheader('Current Teams')
+    st.dataframe(current_team_customers)
+
+    st.subheader('Revenue from Stripe')
     stripe_revenue = brex_transaction_data[(brex_transaction_data['amount'] >= 0) & (brex_transaction_data['description'] == 'STRIPE - TRANSFER')]
-    st.plotly_chart(px.bar(stripe_revenue, x='month', y='amount', title='Stripe Revenue'))
+    st.plotly_chart(px.bar(stripe_revenue, x='month', y='amount', title='Revenue from Stripe'))
 
+
+with expense_tab:
     expenses = brex_transaction_data[brex_transaction_data['amount'] < 0].copy()
     expenses['amount'] = expenses['amount'] * -1
     summed_expenses = expenses.groupby('month').sum(numeric_only=True).reset_index()
     st.plotly_chart(px.bar(summed_expenses, x='month', y='amount', title='Expenses'))
 
-    payroll_expenses = brex_transaction_data[(brex_transaction_data['amount'] < 0) & (brex_transaction_data['description'] == 'RIPPLING - PAYROLL')].copy()
+    payroll_expenses = brex_transaction_data[(brex_transaction_data['amount'] < 0) & (brex_transaction_data['description'].str.contains('RIPPLING'))].copy()
     payroll_expenses['amount'] = payroll_expenses['amount'] * -1
     summed_payroll_expenses = payroll_expenses.groupby('month').sum(numeric_only=True).reset_index()
     st.plotly_chart(px.bar(summed_payroll_expenses, x='month', y='amount', title='Payroll Expenses'))
@@ -205,7 +270,7 @@ with financial_tab:
     st.write('This section calculates a variety of metrics to show our runway. It allows you to configure some assumptions about our finances, and calculates how long we are alive.')
     st.subheader('Runway Assumptions')
 
-    number_months = st.slider('Number of Months to Consider', min_value=1, max_value=6)
+    number_months = st.slider('Number of Months to Consider', min_value=1, max_value=12)
 
     current_yearly_salary = summed_payroll_expenses['amount'].iloc[-1] / 3 * 12
     new_yearly_salary = st.number_input('Yearly Salary (defaults to current)', value=current_yearly_salary, step=1000.0)
